bc1a9043f8378b76048b28f694dc42de82ed9f86
Magnus <magnus.christ@gmx.de>
2022-12-23 12:52:18 +0100

    renamed:    source/bootloader/main.py -> bootloader/main.py 	renamed:    source/lib/logging.py -> lib/logging.py 	renamed:    source/lib/micropython_logging-0.3.dist-info/METADATA -> lib/micropython_logging-0.3.dist-info/METADATA 	renamed:    source/lib/micropython_logging-0.3.dist-info/RECORD -> lib/micropython_logging-0.3.dist-info/RECORD 	renamed:    source/lib/micropython_mqtt-0.7b4.dist-info/METADATA -> lib/micropython_mqtt-0.7b4.dist-info/METADATA 	renamed:    source/lib/micropython_mqtt-0.7b4.dist-info/RECORD -> lib/micropython_mqtt-0.7b4.dist-info/RECORD 	renamed:    source/lib/micropython_operator-0.1.1.dist-info/METADATA -> lib/micropython_operator-0.1.1.dist-info/METADATA 	renamed:    source/lib/micropython_operator-0.1.1.dist-info/RECORD -> lib/micropython_operator-0.1.1.dist-info/RECORD 	renamed:    source/lib/mqtt_async.py -> lib/mqtt_async.py 	renamed:    source/lib/operator.py -> lib/operator.py 	renamed:    source/Kalman.py -> src/Kalman.py 	renamed:    source/boot.py -> src/boot.py 	renamed:    source/conversions.py -> src/conversions.py 	new file:   src/cred.py 	renamed:    source/crypto_keys.py -> src/crypto_keys.py 	renamed:    source/duo_control.py -> src/duo_control.py 	renamed:    source/imu.py -> src/imu.py 	renamed:    source/inetboxapp.py -> src/inetboxapp.py 	renamed:    source/lin.py -> src/lin.py 	renamed:    source/main.py -> src/main.py 	renamed:    source/set_credentials_encrypt.py -> src/set_credentials_encrypt.py 	renamed:    source/spiritlevel.py -> src/spiritlevel.py 	renamed:    source/tools.py -> src/tools.py 	renamed:    source/truma_serv.py -> src/truma_serv.py 	renamed:    source/vector3d.py -> src/vector3d.py

---
+++ src/cred.py
@@ -0,0 +1,70 @@
+# MIT License
+#
+# Copyright (c) 2022  Dr. Magnus Christ (mc0110)
+#
+# This is part of the wifimanager package
+# 
+# 
+# For the proper functioning of the connect-library, the keys "SSID", "WIFIPW", "HOSTNAME" should be included.
+# Any other keys can be added
+
+
+def set_cred_json():
+    import connect
+
+    j = {
+     "SSID": ["text", "SSID:", "1"],
+     "WIFIPW": ["password", "Wifi passcode:", "2"],
+     "MQTT": ["text", "Boker name/IP:", "3"],
+     "UN": ["text", "Broker User:", "4"],
+     "UPW": ["text", "Broker password:", "5"],
+     "HOSTNAME": ["text", "Hostname:", "6"],
+     "ADC": ["checkbox", "Addon DuoControl :", "7"]
+     "ASL": ["checkbox", "Addon SpiritLevel:", "8"],
+#     "OSR": ["checkbox", "OS Web:", "9"],
+     }
+    
+
+    w=connect.Wifi()
+    w.write_cred_json(j)
+
+
+def update_repo():
+    import time, os
+    import mip
+    #sleep to give some boards time to initialize, for example Rpi Pico W
+    time.sleep(3)
+
+    # bootloader for the whole suite
+    tree = "github:mc0110/inetbox2mqtt"
+
+    env = [
+        ["/lib/", "logging.py", "/lib"],
+        ["/lib/", "mqtt_async.py", "/lib"],       
+        ["/lib/", "nanoweb.py", "/lib"],
+        ["/lib/", "crypto_keys.py", "/lib"],
+        ["/lib/", "connect.py", "/lib"],
+        ["/lib/", "gen_html.py", "/lib"],
+        ["/lib/", "web_os.py", "/lib"],
+        
+        ["/src/", "web_os_run.py", "/"],
+        ["/src/", "cred.py", "/"],
+        ["/src/", "boot.py", "/"],
+        ["/src/", "main.py", "/"],
+        ["/src/", "main1.py", "/"],
+        
+        ["/src/", "tools.py", "/"],
+        ["/src/", "conversions.py", "/"],
+        ["/src/", "lin.py", "/"],
+        ["/src/", "inetboxapp.py", "/"],
+        ["/src/", "truma_serv.py", "/"],
+        ["/src/", "duo_control.py", "/"],
+        ["/src/", "spiritlevel.py", "/"],
+        ["/src/", "imu.py", "/"],
+        ["/src/", "Kalman.py", "/"],
+        ["/src/", "vector3d.py", "/"],
+        ]
+
+
+    for i in range(len(env)):
+        mip.install(tree+env[i][0]+env[i][1], target= env[i][2])
