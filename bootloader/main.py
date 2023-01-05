702f71ff9f045a5fce555e7128ebbe110a75fb8f
Magnus <magnus.christ@gmx.de>
2023-01-05 17:21:08 +0100

    modified:   bootloader/main.py

--- bootloader/main.py
+++ bootloader/main.py
@@ -40,9 +40,9 @@ tree = "github:mc0110/inetbox2mqtt"
 # mip.install(tree)
 
 env = [       
-    ["/lib/", "crypto_keys.py", "/lib"],
-    ["/lib/", "connect.py", "/lib"],
-    ["/src/", "tools.py", "/"],
+#    ["/lib/", "crypto_keys.py", "/lib"],
+#    ["/lib/", "connect.py", "/lib"],
+#    ["/src/", "tools.py", "/"],
     ["/src/", "cred.py", "/"],
     ]
 
