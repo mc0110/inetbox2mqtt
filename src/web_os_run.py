a4a8d4366d37e4a823b612d2533f6067943a4d0c
Magnus <magnus.christ@gmx.de>
2023-01-11 23:17:34 +0100

    modified:   src/web_os_run.py

--- src/web_os_run.py
+++ src/web_os_run.py
@@ -7,7 +7,7 @@
 # This snippet should you include in your software project to use the wifi manager 
 
 
-import web_os as os
+import lib.web_os as os
 from nanoweb import Nanoweb
 import uasyncio as asyncio
 
@@ -19,31 +19,10 @@ def run(w):
     # debug=True for debugging
     naw = Nanoweb(80) #, debug = True)
     connect = w
-    os.init(connect)
+    os.init(connect, naw)
 
     naw.STATIC_DIR = "/"
-    naw.routes = {
-         '/': os.index,
-         '/s': os.status,
-         '/loop': os.loop,
-         '/ta': os.toggle_ap,
-         '/ts1': os.set_sta,
-         '/ts': os.toggle_sta,
-         '/rm': os.toggle_run_mode,
-         '/wc': os.creds,
-         '/scan':os.scan_networks,
-         '/cp': os.cp,
-         '/ur': os.ur,
-         '/ur1': os.ur1,
-         '/dc': os.del_cred,
-         '/sc': os.swp_cred,
-         '/rc': os.res_cred,
-         '/rb': os.s_reboot,
-         '/rb1': os.h_reboot,
-         '/upload/*': os.upload,
-         '/fm*': os.fm,
-         '/dir*': os.set_dir,
-     }
+
 
     loop = asyncio.get_event_loop()
     loop.create_task(naw.run())
