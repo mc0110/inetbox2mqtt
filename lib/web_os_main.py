52fe6814f9860d39eaa86332029482b26df6fda4
mc0110 <magnus.christ@gmx.de>
2023-08-14 09:47:05 +0200

    modified:   lib/web_os_main.py

--- lib/web_os_main.py
+++ lib/web_os_main.py
@@ -60,34 +60,18 @@ def run(w, lin_debug, inet_debug, webos_debug, naw_debug, logfile):
         serial = UART(w.p.get_data("lin_uart"), baudrate=9600, bits=8, parity=None, stop=1, timeout=3, rx=Pin(w.p.get_data("lin_rx")), tx=Pin(w.p.get_data("lin_tx"))) # this is the HW-UART-no 2
     if (w.platform=="esp32"):
         serial = UART(w.p.get_data("lin_uart"), baudrate=9600, bits=8, parity=None, stop=1, timeout=3, rx=w.p.get_data("lin_rx"), tx=w.p.get_data("lin_tx")) # this is the HW-UART-no 2    
-    
-#     if (w.platform == "esp32"):
-#         
-#         log.info("Found ESP32 Board, using UART2 for LIN on GPIO 16(rx), 17(tx)")
-#         # ESP32-specific hw-UART (#2)
-#         serial = UART(2, baudrate=9600, bits=8, parity=None, stop=1, timeout=3) # this is the HW-UART-no 2
-#     elif (w.platform == "rp2"):
-#         # RP2 pico w -specific hw-UART (#2)
-#         log.info("Found Raspberry Pico Board, using UART1 for LIN on GPIO 4(tx), 5(rx)")
-#         serial = UART(1, baudrate=9600, tx=Pin(4), rx=Pin(5), timeout=3) # this is the HW-UART1 in RP2 pico w
-#     else:
-#         log.debug("No compatible Board found!")
-        
+            
     # Initialize the lin-object
     lin = Lin(serial, w.p, lin_debug, inet_debug)
     os.init(w, lin, naw, webos_debug, logfile)
 
     naw.STATIC_DIR = "/"
     
-#    connect.config.set_last_will("service/truma/control_status/alive", "OFF", retain=True, qos=0)  # last will is important
-#    connect.set_proc(subscript = callback, connect = conn_callback)
-
-
     loop = asyncio.get_event_loop()
     log.info("Start nanoweb server")
     loop.create_task(naw.run())
     loop.create_task(lin_loop())
-    loop.create_task(mqtt_loop())
+#    loop.create_task(mqtt_loop())
     log.info("Start OS command loop")
     loop.create_task(os.command_loop())
     loop.run_forever()        
